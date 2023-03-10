70adb50be5b12cd39606900a538cd4cba04e79a4
DocWilco <github@drwil.co>
2023-03-10 16:04:15 +0100

    Don't try to do the git bits, and add output file param

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,17 @@ use clap::Parser;
 use itertools::Itertools;
 use serde::Deserialize;
 use serde_json::Value;
-use std::{io::Write, path::Path, process::Command};
+use std::{io::Write, path::Path};
 
 #[derive(Parser, Debug)]
 #[command(author="DocWilco <github@drwil.co>", version, about="Turn cache-domains git repo into VCL", long_about=None)]
 struct Args {
-    /// The git repo to clone
-    #[clap(
-        short,
-        long,
-        default_value = "https://github.com/uklans/cache-domains.git"
-    )]
-    repo: String,
-    /// Work directory, where the repo will be cloned to and cachedomains.vcl
-    /// will be written to
+    /// Directory where the repo is
     #[clap(short, long, default_value = "cachedomains")]
-    work_dir: String,
+    repo_dir: String,
+    /// Output file, defaults to stdout
+    #[clap(short, long)]
+    output: Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -30,46 +25,7 @@ struct CacheDomain {
 fn main() {
     let args = Args::parse();
 
-    // Create work directory if it doesn't exist
-    let work_dir = Path::new(&args.work_dir);
-    if !Path::new(&work_dir).exists() {
-        std::fs::create_dir(&work_dir).unwrap();
-    }
-    // Create repo directory if it doesn't exist
-    let repo_dir = work_dir.join("repo");
-    let repo_exists = repo_dir.exists();
-    if !repo_exists {
-        std::fs::create_dir(&repo_dir).unwrap();
-    }
-
-    // If repo directory existed already, try a git pull, if that fails,
-    // or if the repo directory didn't exist, try a git clone.
-    let pull_failed_or_dir_didnt_exist = !repo_exists || {
-        println!("repo directory exists, trying git pull");
-        let mut git_pull = Command::new("git")
-            .arg("pull")
-            .current_dir(&repo_dir)
-            .spawn()
-            .expect("failed to execute process");
-        let git_pull_status = git_pull.wait().expect("failed to wait on child");
-        if !git_pull_status.success() {
-            println!("git pull failed");
-        }
-        !git_pull_status.success()
-    };
-    if pull_failed_or_dir_didnt_exist {
-        println!("trying git clone");
-        let mut git_clone = Command::new("git")
-            .arg("clone")
-            .arg(args.repo)
-            .arg(&repo_dir)
-            .spawn()
-            .expect("failed to execute process");
-        let git_clone_status = git_clone.wait().expect("failed to wait on child");
-        if !git_clone_status.success() {
-            panic!("git clone failed");
-        }
-    }
+    let repo_dir = Path::new(&args.repo_dir);
 
     // Read cache_domains.json
     let json_file = repo_dir.join("cache_domains.json");
@@ -149,8 +105,13 @@ sub set_cache_domain {
 }"#,
     );
     // Open output file for writing
-    let vcl_file = work_dir.join("cachedomains.vcl");
-    let mut output_file = std::fs::File::create(vcl_file).unwrap();
-    output_file.write_all(vcl.as_bytes()).unwrap();
-    println!("Wrote cache_domains/cache_domains.vcl")
+    let mut output: Box<dyn Write> = if let Some(ref output) = args.output {
+        Box::new(std::fs::File::create(output).unwrap())
+    } else {
+        Box::new(std::io::stdout())
+    };
+    output.write_all(vcl.as_bytes()).unwrap();
+    if let Some(output) = args.output {
+        eprintln!("Wrote {}", output);
+    }
 }
